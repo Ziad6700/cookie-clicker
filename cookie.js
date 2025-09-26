0be9a2634095d65efa9baf40cf46f31a69e8ea88
Ziad6700 <9025547@student.zadkine.nl>
2025-09-26 13:31:52 +0200

    prototype version 0.2
    click upgrade werkt

--- cookie.js
+++ cookie.js
@@ -1,24 +1,41 @@
 let cookieTotal = 0;
 
 class clicker{
-    // cookieClicker;
+    cps = 1;
 
     constructor () {
         // this.cookieClicker = cookieClicker;
     }
 
     plusCookies(){
-        cookieTotal += 1;
+        cookieTotal += this.cps;
         console.log(cookieTotal);
         document.getElementById("counter").textContent = cookieTotal;
     }
     
 }
 
-class upgrades{
-    cookiePerSecond;
-    
+class upgrades{  
+   
+    constructor (cookiePerSecond) {
+        cookiePerSecond
+    }
+
+    dfghj(){
+
+        if (cookieTotal >= 5) {
+            cookieClicker1.cps += 2;
+            cookieTotal -= 5;
+             document.getElementById("counter").textContent = cookieTotal;
+        } else {
+            console.log ("niet genoeg cookies");
+        }
+    }
 }
 
 let cookieClicker1 = new clicker();
 cookieClicker1.plusCookies();
+
+let yupgrade1 = new upgrades();
+yupgrade1.cookiePerSecond = 2;
+yupgrade1.dfghj();
