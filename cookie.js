582e03fb876f6e3dd442303d41609eb43c499195
Ziad6700 <9025547@student.zadkine.nl>
2025-10-03 15:02:38 +0200

    prototype version 4.0
    auto regeneration voor cookies en je kan dingen zoals grandmas nu kopen en de items (auto regeneration werken ook correct)

--- cookie.js
+++ cookie.js
@@ -1,6 +1,15 @@
-let cookieTotal = 0;
+class ui{
+    counter = 0;
 
-class clicker{
+    add(amount){
+        this.counter += amount;
+        document.getElementById("counter").textContent = this.counter;
+    }
+}
+
+let cookieTotal = new ui();
+
+class Clicker{
     cps = 1;
 
     constructor () {
@@ -8,39 +17,73 @@ class clicker{
     }
 
     plusCookies(){
-        cookieTotal += this.cps;
-        console.log(cookieTotal);
-        document.getElementById("counter").textContent = cookieTotal;
+        // cookieTotal += this.cps;
+        cookieTotal.add(this.cps);
+        // console.log(cookieTotal);
+        // document.getElementById("counter").textContent = cookieTotal;
     }
     
 }
 
-class upgrades{  
-   
+class Upgrades{  
+    $upgradeAmount = 2;
     constructor (cookiePerSecond, price) {
-        this.cookiePerSecond = cookiePerSecond;
+        // this.cookiePerSecond = cookiePerSecond;
         this.price = price;
     }
 
     dfghj(){
 
-        if (cookieTotal >= this.price) {
-            cookieClicker1.cps += 2;
-            cookieTotal -= this.price;
+        if (cookieTotal.counter >= this.price) {
+            // cookieClicker1.cps += 2;
+            cookieTotal.add(-this.price);
             this.price = Math.floor(this.price * 1.5);
-            cookieClicker1.cps = Math.floor(cookieClicker1.cps * 1.2);
+            cookieClicker1.cps += Math.floor(this.$upgradeAmount);
+            this.$upgradeAmount = this.$upgradeAmount * 1.2;
 
-             document.getElementById("counter").textContent = cookieTotal;
-             document.getElementById("priceClicker").textContent = "price:" + this.price;
+            //  document.getElementById("counter").textContent = cookieTotal;
+             document.getElementById("priceClicker").textContent = "clicker upgrade:" + this.price;
         } else {
             console.log ("niet genoeg cookies");
         }
     }
 }
 
-let cookieClicker1 = new clicker();
-cookieClicker1.plusCookies();
+class AutoGenerater{
+    cps;
+    amount = 0;
+    price;
 
-let upgrade1 = new upgrades(2, 5);
+    constructor(cps, price){
+        this.cps = cps;
+        this.price = price;
+        setInterval(() => this.generate(), 1000);
+    }
 
+    buy(){
+        //prirc check
+        //amount++
+        if (cookieTotal.counter >= this.price) { 
+            this.amount += 1;
+            cookieTotal.add(-this.price);
+            this.price = Math.floor(this.price * 1.5);
+                  document.getElementById("generator").textContent = "grandma:" + this.price;
+    } else {
+        console.log("niet genoeg");
+    }
+}
+
+    generate() {
+        cookieTotal.add(this.cps * this.amount);
+        // document.getElementById("counter").textContent = cookieTotal;
+    }
+}
+
+let cookieClicker1 = new Clicker();
+// cookieClicker1.plusCookies();
+
+let upgrade1 = new Upgrades(2, 5);
 upgrade1.dfghj();
+
+let grandma = new AutoGenerater(3, 5);
+grandma.buy();
